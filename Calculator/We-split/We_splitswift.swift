3271424bbefdb8004d7317b5c8261ee2b4ac68f2
boyko13 <81235503+boyko13@users.noreply.github.com>
2024-05-06 21:40:22 +0200

    Add files via upload

---
+++ Calculator/We-split/We_splitswift.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Boyo on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
